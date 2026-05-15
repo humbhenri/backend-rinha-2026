93f13e9a3df998df9c05d76606ae049d6ec275e7
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2026-05-15 18:42:08 -0300

    add fraud score endpoint

---
+++ REAME.md
@@ -0,0 +1,4 @@
+# Rinha de Backend 2026
+
+- https://github.com/zanfranceschi/rinha-de-backend-2026
+- https://github.com/zanfranceschi/rinha-de-backend-2026/blob/main/docs/en/README.md
--- app/main.go
+++ app/main.go
@@ -3,23 +3,66 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
-// Response define a estrutura do JSON de retorno
-type Response struct {
-	Message string `json:"message"`
+type Transaction struct {
+	Amount float32 `json:"amount"`
+	Installments int `json:"installments"`
+	RequestedAt time.Time `json:"requested_at"`
 }
 
-// helloHandler manipula as requisições HTTP na rota principal
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// Define o cabeçalho para retornar o formato JSON
+type Customer struct {
+	AvgAmount float32 `json:"avg_amount"`
+	TxCount24h int `json:"tx_count_24h"`
+	KnownMerchants []string `json:"known_merchants"`
+}
+
+type Merchant struct {
+	Id string `json:"id"`
+	Mcc string `json:"mcc"`
+	AvgAmount float32 `json:"avg_amount"`
+}
+
+type Terminal struct {
+	IsOnline bool `json:"is_online"`
+	CardPresent bool `json:"card_present"`
+	KmFromHome float32 `json:"km_from_home"`
+}
+
+type LastTransaction struct {
+	Timestamp time.Time `json:"timestamp"`
+	KmFromCurrent float32 `json:"km_from_current"`
+}
+
+type Payload struct {
+	Id string `json:"id"`
+	Transaction Transaction `json:"transaction"`
+	Customer Customer `json:"customer"`
+	Merchant Merchant `json:"merchant"`
+	Terminal Terminal `json:"terminal"`
+	LastTransaction LastTransaction `json:"last_transaction"`
+}
+
+type Response struct {
+	Approved bool `json:"approved"`
+	FraudScore float32 `json:"fraud_score"`
+}
+
+func fraudScoreHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var p Payload
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-
-	// Define o status HTTP 200 OK
 	w.WriteHeader(http.StatusOK)
-
-	// Instancia e envia a resposta formatada
-	response := Response{Message: "Hello, World!"}
+	response := Response{}
 	json.NewEncoder(w).Encode(response)
 }
 
@@ -28,9 +71,7 @@ func readyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Vincula a rota raiz "/" à função do manipulador
 	http.HandleFunc("/ready", readyHandler)
-
-	// Inicia o servidor HTTP local na porta 8080
+	http.HandleFunc("/fraude-score", fraudScoreHandler)
 	http.ListenAndServe(":6969", nil)
 }
